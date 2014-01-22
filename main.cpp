10c5339ce5007acffe0a7742537f73f438866a70
Enzhaev Ivan <8observer8@gmail.com>
2014-01-22 16:31:56 +0400

    Дабавил строку для вывода "Hello, World!" на экран

--- main.cpp
+++ main.cpp
@@ -6,14 +6,10 @@
  */
 
 #include <cstdlib>
-
+#include <iostream>
 using namespace std;
 
-/*
- * 
- */
 int main(int argc, char** argv) {
-
+    cout << "Hello, World!" << endl;
     return 0;
 }
-

f2760ec7b672d3699d6d8575552fd9c6df9ae947
Enzhaev Ivan <8observer8@gmail.com>
2014-01-22 15:16:05 +0400

    First Commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Ivan
+ *
+ * Created on January 22, 2014, 3:14 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
